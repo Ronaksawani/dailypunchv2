480a0645c24219edbe7ed2ee779d02c521c0be09
RONAK-JAYSUKH-SAWANI <96838705+Ronaksawani@users.noreply.github.com>
2024-04-16 06:39:25 +0530

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -17,7 +17,7 @@ import exit from "../assets/images/exit.png";
 
 import equity from "../assets/images/equity.png";
 import DashMarginChart from "./dash_margin_chart";
-import ReactHTMLTableToExcel from "react-html-table-to-excel";
+//import ReactHTMLTableToExcel from "react-html-table-to-excel";
 import add from "../assets/images/icons8-add-new-50.png";
 import done from "../assets/images/icons8-done-64.png";
 import no_watchlist_img from "../assets/images/no_watchlist.png";
@@ -1499,14 +1499,14 @@ function Dashboard() {
                         Trade history
                       </div>
 
-                      <ReactHTMLTableToExcel
-                        id="downloadBtn"
-                        table="table-to-xls"
-                        filename="Trade-History"
-                        sheet="Trade-history"
-                        className="fa fa-download"
-                        buttonText=" Download"
-                      />
+                      // <ReactHTMLTableToExcel
+                      //   id="downloadBtn"
+                      //   table="table-to-xls"
+                      //   filename="Trade-History"
+                      //   sheet="Trade-history"
+                      //   className="fa fa-download"
+                      //   buttonText=" Download"
+                      // />
 
                       {/* <button
                         id="downloadBtn"
